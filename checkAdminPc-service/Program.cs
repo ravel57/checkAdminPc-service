e4cf1ca27dded12ef8693c5f5f9956d10a085e76
plomakin <plomakin@REMC-ADMIN>
2021-08-04 15:16:11 +0300

    Добавьте файлы проекта.

---
+++ checkAdminPc-service/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkAdminPc_service {
+    static class Program {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main(string[] args) {
+            if(Environment.UserInteractive) {
+                try {
+                    var appPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                    System.Configuration.Install.ManagedInstallerClass.InstallHelper(new string[] { appPath });
+                    ServiceController sc = new ServiceController("checkAdminPC");
+                    sc.Start();
+                    Console.WriteLine("\nInstalled successfully\n");
+                } catch(Exception ex) { Console.WriteLine(ex.Message); }
+                if(args != null && args.Length > 0) {
+                    switch(args[0]) {
+                        case "--uninstall":
+                            try {
+                                var appPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                                System.Configuration.Install.ManagedInstallerClass.InstallHelper(new string[] { "/u", appPath });
+                                Console.WriteLine("Unnstalled successfully");
+                            } catch(Exception ex) { Console.WriteLine(ex.Message); }
+                            break;
+                    }
+                }
+            } else {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] {
+                    new checkAdminPC()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
---
+++ checkAdminPc-service/checkAdminPC.cs
@@ -0,0 +1,84 @@
+﻿using Cassia;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace checkAdminPc_service {
+    class Settings {
+        public string key;
+        public string serverUrl;
+        public string computerName;
+    }
+
+
+    public partial class checkAdminPC : ServiceBase {
+
+        static string setJson = File.ReadAllText(".\\set.json");
+        static Settings settings = JsonConvert.DeserializeObject<Settings>(setJson);
+
+        public checkAdminPC() {
+            InitializeComponent();
+        }
+
+        void exec() {
+            ITerminalServer server = new TerminalServicesManager().GetLocalServer();
+            server.Open();
+            ITerminalServicesSession session = server.GetSessions().First(s => s.ConnectionState == Cassia.ConnectionState.Active);
+            if(session != null) {
+
+                string userName = session.UserAccount.Value.Remove(0, 1 + session.DomainName.Length);
+                if(!settings.serverUrl.EndsWith("/")) settings.serverUrl += '/';
+                string url = new StringBuilder(settings.serverUrl).Append(settings.computerName).ToString();
+                string json = new StringBuilder("{\"key\": \"").Append(settings.key).Append("\", \"lastLoggedUser\": \"").Append(userName).Append("\"}").ToString();
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest) WebRequest.Create(url);
+                httpWebRequest.ContentType = "application/json; charset=utf-8";
+                httpWebRequest.Method = "POST";
+                //Console.WriteLine(json);
+
+                using(var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream())) {
+                    streamWriter.Write(json);
+                }
+                using(var response = httpWebRequest.GetResponse()) {}
+                //HttpWebResponse httpRes = (HttpWebResponse) httpWebRequest.GetResponse();
+            }
+        }
+
+
+        protected override void OnStart(string[] args) {
+            Task.Run(() => {
+                while(true) {
+                    try {
+                        exec();
+                    } catch(WebException e) {
+                        settings = JsonConvert.DeserializeObject<Settings>(setJson);
+                        //EventLog.WriteEntry("WebException: " + e.Message, EventLogEntryType.Error);
+                    } catch(InvalidOperationException e) {
+                        settings = JsonConvert.DeserializeObject<Settings>(setJson);
+                        //EventLog.WriteEntry("InvalidOperationException: " + e.Message, EventLogEntryType.Error);
+                    } catch(Exception e) {
+                        settings = JsonConvert.DeserializeObject<Settings>(setJson);
+                        //EventLog.WriteEntry("Exception: " + e.Message, EventLogEntryType.Error);
+                    } finally {
+                        Thread.Sleep(20000);
+                    }
+                }
+            });
+
+        }
+
+        protected override void OnStop() {
+            //EventLog.WriteEntry("We did it! Stoped", EventLogEntryType.Information);
+        }
+    }
+}
